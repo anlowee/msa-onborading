e354fcc2de8d3ed19f22563f5e532abe71008e7c
Xiaochong(Eddy) Wei <40865608+anlowee@users.noreply.github.com>
2024-06-06 22:59:06 +0000

    customized

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Xiàochōng Wèi",
+  title: "MASc Student at University of Toronto ECE Department",
+  email: "xiaochong.wei@mail.utoronto.ca",
+  gitHub: "anlowee",
+  instagram: "realeddywei",
+  linkedIn: "Xiaochong Wei",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "eddywei22",
+  youTube: "eddywei",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,17 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a MASc student in the Department of Electrical and Computer Engineering (ECE) at the University of Toronto. My supervisor is Ding Yuan.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C/C++, Java, python",
+  "Adroid development",
+  "Operating systems",
+  "Performance optimization/diagnostics",
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My research interests include performance and failure diagnosis and optimization in software systems.";
 
 const About = () => {
   return (
